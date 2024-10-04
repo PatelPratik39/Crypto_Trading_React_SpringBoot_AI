f0e0e0c3960361a46bbe68fd43a76acfec0641fb
PatelPratik39 <ptl.pratik717@gmail.com>
2024-10-04 14:42:04 -0500

    Initial App is up and running

--- trading/src/main/java/com/fullstck/TradingApplication.java
+++ trading/src/main/java/com/fullstck/TradingApplication.java
@@ -8,6 +8,7 @@ public class TradingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TradingApplication.class, args);
+        System.out.println("Trading App is up and Running!!!!");
     }
 
 }
